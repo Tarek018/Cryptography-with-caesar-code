760539aa5a00c752ff099680fcba0eab86c34775
Tarek <tarek22042002triki@gmail.com>
2023-05-07 22:03:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
 alphabet = []
-
-dictionnaire = {}
-
 Text_chiffrer = ""
 
-espace = {}
-
-space_indices = []
 
 
 
-# Ajouter chaque lettre de l'alphabet au tableau en utilisant une boucle for
+# Add each letter of the alphabet to the array using a for loop
 for letter in range(ord('A'), ord('Z') + 1):
     alphabet.append(chr(letter))
 
-# Afficher le tableau
-print('La cle de chiffrement est trois')
-Text_Claire=input('donner le text claire : ')
-i=0
-# print(alphabet)
-Text_Claire = Text_Claire
-for char in Text_Claire.upper():
+print('The encryption key is three')
+Text_Claire=input('Give the clear text: ')
+
+Text_Claire = Text_Claire.upper()
+for char in Text_Claire:
     i=0
-    for letter in alphabet:
-        if letter == char:
-            dictionnaire[char]=i
-        i=i+1
-
-for j in range(len(Text_Claire)):
-    if Text_Claire[j] == " ":
-        space_indices.append(j)
-
-for key,value in dictionnaire.items():
-    print(alphabet[value] )
-    if alphabet[value] == 'X':
-         Text_chiffrer = Text_chiffrer + alphabet[0]
-    else:
-         if alphabet[value] == 'Y':
-             Text_chiffrer = Text_chiffrer + alphabet[1]
-         else:
-             if alphabet[value] == 'Z':
-                 Text_chiffrer = Text_chiffrer + alphabet[2]
-             else:
-                Text_chiffrer = Text_chiffrer + alphabet[value+3]
-    
-print(Text_chiffrer)
+    for ch in alphabet:
+        if char == ch and char =="X":
+            Text_chiffrer = Text_chiffrer + alphabet[0]    
+            break
+        else:
+            if char == ch and char == 'Y':
+              Text_chiffrer = Text_chiffrer + alphabet[1]
+              break
+            else:
+                if char == ch and char == 'Z':
+                    Text_chiffrer = Text_chiffrer + alphabet[2]
+                    break
+                else:
+                    if char == ch:
+                        Text_chiffrer = Text_chiffrer + alphabet[i+3]
+        i=i+1       
+print("Cipher text is : "+Text_chiffrer)
