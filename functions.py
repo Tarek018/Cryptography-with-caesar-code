f8f57cefc6b05eb79f07ca91b01188b0f72924c7
Tarek <tarek22042002triki@gmail.com>
2023-05-07 22:53:58 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,11 +3,13 @@ def caesar_code(Text):
     # Add each letter of the alphabet to the array using a for loop
     for letter in range(ord('A'), ord('Z') + 1):
         alphabet.append(chr(letter))
+
     Text_chiffrer = ""
+
     for char in Text:
         i=0
         for ch in alphabet:
-            if char == ch and char =="X":
+            if char == ch and char == "X":
                 Text_chiffrer = Text_chiffrer + alphabet[0]    
                 break
             else:
@@ -21,5 +23,5 @@ def caesar_code(Text):
                     else:
                         if char == ch:
                             Text_chiffrer = Text_chiffrer + alphabet[i+3]
-            i=i+1     
+            i = i+1     
     return Text_chiffrer
